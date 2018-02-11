024dea105e63f4df4a437c7452087a1caf650a0f
Bernd Zuther <bernd.zuther@codecentric.de>
2018-02-11 14:58:55 +0100

    fix connect script

--- connect_to_slave.sh
+++ connect_to_slave.sh
@@ -3,7 +3,7 @@
 while [ true ]
 do
     sleep 20
-    SCRIPT_NAME+="/get_$JENKINS_AGENT_NAME"
+    SCRIPT_NAME="/get_$JENKINS_AGENT_NAME"
     SCRIPT_NAME+="_secret.groovy"
     SECRET=$(curl -s --user 'admin:admin' --data-urlencode "script=$(< $SCRIPT_NAME)" $JENKINS_URL/scriptText)
 
