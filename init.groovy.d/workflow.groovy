504326d5c454b9f7c66e7e922fc7c601220ab2b8
Bernd Zuther <bernd.zuther@codecentric.de>
2018-02-11 20:02:08 +0100

    change job name

--- init.groovy.d/workflow.groovy
+++ init.groovy.d/workflow.groovy
@@ -4,7 +4,7 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob
 import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
 import hudson.plugins.git.GitSCM
 
-WorkflowJob job = Jenkins.instance.createProject(WorkflowJob, 'Jenkins')
+WorkflowJob job = Jenkins.instance.createProject(WorkflowJob, 'Jenkins Pipeline')
 
 def definition = new CpsScmFlowDefinition(new GitSCM('https://github.com/zutherb/eea.docker.jenkins.git'), 'Jenkinsfile')
 job.definition = definition
